3f8ed478dcae0f53ffde524351023f77d691f85a
Alvaro Elizalde Romero <100690046+aalvaro0303@users.noreply.github.com>
2023-04-17 23:45:46 +0200

    modificarmarca

--- pruebaCo/src/Presentacion/MarcaPresentacion/GUIModificarMarca.java
+++ pruebaCo/src/Presentacion/MarcaPresentacion/GUIModificarMarca.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package Presentacion.MarcaPresentacion;
 
@@ -8,60 +8,66 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-
-
+import Negocio.MarcaNegocio.TMarca;
 import Presentacion.Controlador.Controlador;
 import Presentacion.Controlador.Evento;
 import Presentacion.GUI.GUI;
 
-/** 
+/**
 * <!-- begin-UML-doc -->
 * <!-- end-UML-doc -->
 * @author sagog
 * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 */
 public class GUIModificarMarca extends JFrame implements GUI {
-	/** 
+	/**
 	* <!-- begin-UML-doc -->
 	* <!-- end-UML-doc -->
 	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	public GUIModificarMarca() {
-		setTitle("Alta Marca");
-		JPanel panel=new JPanel();
-		JLabel id=new JLabel("Identificador:");
-		final JTextField tNombre= new JTextField(20);
-		JButton aceptar=new JButton("Aceptar:");
-		JButton cancelar=new JButton("Cancelar");
-		
-		panel.add(id);
-		panel.add(tNombre);
-		panel.add(aceptar);
-		panel.add(cancelar);
-		getContentPane().add(panel);
-		pack();
-		
-		aceptar.addActionListener(new ActionListener()
+			setTitle("Alta Marca");
+			JPanel panel=new JPanel();
+			JLabel lId=new JLabel("Identificador:");
+			final JTextField tId= new JTextField(10);
+			JLabel lNombre=new JLabel("Nombre: ");
+			final JTextField tNombre= new JTextField(20);
+			JButton aceptar=new JButton("Aceptar:");
+			JButton cancelar=new JButton("Cancelar");
+			
+			panel.add(lId);
+			panel.add(lNombre);
+			panel.add(aceptar);
+			panel.add(cancelar);
+			getContentPane().add(panel);
+			pack();
+			
+			aceptar.addActionListener(new ActionListener()
 			{ public void actionPerformed(ActionEvent e)
-				{		
+				{
 					setVisible(false);
-					String nombre= tNombre.getText();
-					int id=Integer.parseInt(nombre);
-					Controlador.getInstancia().accion(Evento.MODIFICAR_MARCA, new Integer(id));
+					TMarca tmarca=new TMarca(tNombre.getText(),Integer.parseInt(tId.getText()), 0,0);
+					Controlador.getInstancia().accion(Evento.MODIFICAR_MARCA, tmarca);
 				}
-		});
-		cancelar.addActionListener(new ActionListener()
+			});
+			cancelar.addActionListener(new ActionListener()
 			{ public void actionPerformed(ActionEvent e)
-				{		
+				{
 					setVisible(false);
-			}
-		});
-		
+				}
+			});
+			
 	}
 
 	@Override
 	public void update(int evento, Object datos) {
-		// TODO Auto-generated method stub
-		
+		if(Evento.MODIFICAR_MARCA==evento){
+			JOptionPane.showMessageDialog(null, "La marca ha sido modificado con exito");
+			this.setVisible(false);
+		}
+		else if(Evento.RES_MODIFICAR_MARCA_KO==evento){
+			JOptionPane.showMessageDialog(null, "No se pudo modificar la marca");
+		}
+	
 	}
 }
